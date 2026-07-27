a124fa35750402354ab71937bb2d142e7c4f59df
thor2002ro <1249056+thor2002ro@users.noreply.github.com>
2026-07-27 11:05:16 +0300

    Local: Add libMPV backend and settings

--- app/src/main/java/org/jellyfin/androidtv/ui/settings/routes.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/settings/routes.kt
@@ -54,6 +54,7 @@ import org.jellyfin.androidtv.ui.settings.screen.playback.PreferredLanguageType
 import org.jellyfin.androidtv.ui.settings.screen.playback.exoPlayerSettingsRoutes
 import org.jellyfin.androidtv.ui.settings.screen.playback.libassSettingsRoutes
 import org.jellyfin.androidtv.ui.settings.screen.playback.libVLCSettingsRoutes
+import org.jellyfin.androidtv.ui.settings.screen.playback.mpvSettingsRoutes
 import org.jellyfin.androidtv.ui.settings.screen.playback.mediasegment.SettingsPlaybackMediaSegmentScreen
 import org.jellyfin.androidtv.ui.settings.screen.playback.mediasegment.SettingsPlaybackMediaSegmentsScreen
 import org.jellyfin.androidtv.ui.settings.screen.playback.nextup.SettingsPlaybackNextUpBehaviorScreen
@@ -318,4 +319,4 @@ val routes = mapOf<String, RouteComposable>(
 			artifactId = context.parameters["artifactId"]!!
 		)
 	},
-) + libassSettingsRoutes + libVLCSettingsRoutes + exoPlayerSettingsRoutes
+) + libassSettingsRoutes + libVLCSettingsRoutes + mpvSettingsRoutes + exoPlayerSettingsRoutes
