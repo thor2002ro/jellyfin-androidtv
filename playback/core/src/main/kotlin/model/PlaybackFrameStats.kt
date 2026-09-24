cff7894cbc3ad1042a966ecc943c2e2c73f88d3b
thor2002ro <1249056+thor2002ro@users.noreply.github.com>
2026-09-24 10:29:25 +0300

    Local: Integrate libass fixes and metrics

--- playback/core/src/main/kotlin/model/PlaybackFrameStats.kt
+++ playback/core/src/main/kotlin/model/PlaybackFrameStats.kt
@@ -1,5 +1,28 @@
 package org.jellyfin.playback.core.model
 
+data class PlaybackLibassStats(
+	val renderCount: Long,
+	val changedRenderCount: Long,
+	val emptyRenderCount: Long,
+	val imageCount: Long,
+	val slowRenderCount: Long,
+	val maxImageCount: Int,
+	val maxBitmapPixels: Long,
+	val totalBitmapPixels: Long,
+	val atlasUploadPageCount: Long,
+	val maxAtlasUploadPageCount: Int,
+	val maxAtlasUploadPagePixels: Long,
+	val totalAtlasUploadPagePixels: Long,
+	val executorTimeoutCount: Long,
+	val supersededRequestCount: Long,
+	val fps: Double,
+	val changedRatio: Double,
+	val averageRenderMs: Double,
+	val minRenderMs: Double,
+	val maxRenderMs: Double,
+	val lastRenderMs: Double,
+)
+
 data class PlaybackFrameStats(
 	val droppedFrames: Int,
 	val corruptedFrames: Int,
@@ -17,6 +40,7 @@ data class PlaybackFrameStats(
 	val subtitleParser: String? = null,
 	val subtitlePath: String? = null,
 	val extractorFlags: String? = null,
+	val libass: PlaybackLibassStats? = null,
 ) {
 	companion object {
 		val EMPTY = PlaybackFrameStats(
---
+++ playback/media3/exoplayer/src/test/kotlin/LibassPlaybackTests.kt
@@ -0,0 +1,19 @@
+package org.jellyfin.playback.media3.exoplayer
+
+import io.github.peerless2012.ass.media.type.AssRenderType
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class LibassPlaybackTests : FunSpec({
+	test("libass disables next item preloading") {
+		canPreloadNextItem(libassEnabled = true) shouldBe false
+		canPreloadNextItem(libassEnabled = false) shouldBe true
+	}
+
+	test("libass performance stats only measure libass renderers") {
+		canMeasureLibassPerformance(libassEnabled = false, renderType = AssRenderType.OVERLAY_OPEN_GL) shouldBe false
+		canMeasureLibassPerformance(libassEnabled = true, renderType = AssRenderType.CUES) shouldBe false
+		canMeasureLibassPerformance(libassEnabled = true, renderType = AssRenderType.OVERLAY_OPEN_GL) shouldBe true
+		canMeasureLibassPerformance(libassEnabled = true, renderType = AssRenderType.OVERLAY_CANVAS) shouldBe true
+	}
+})
