db3ef96f37a907faabe78479688894f4f3efbb48
thor2002ro <1249056+thor2002ro@users.noreply.github.com>
2026-09-24 10:29:50 +0300

    Local: Enable player stats by default in debug builds

--- app/src/main/java/org/jellyfin/androidtv/ui/player/video/VideoPlayerOverlay.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/player/video/VideoPlayerOverlay.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.jellyfin.androidtv.BuildConfig
 import org.jellyfin.androidtv.R
 import org.jellyfin.androidtv.preference.UserPreferences
 import org.jellyfin.androidtv.preference.constant.ZoomMode
@@ -83,7 +84,7 @@ fun VideoPlayerOverlay(
 	val videoQueueManager = koinInject<VideoQueueManager>()
 	val userPreferences = koinInject<UserPreferences>()
 	var pausedOverlayDismissed by remember { mutableStateOf(false) }
-	var showPlaybackInfo by remember { mutableStateOf(false) }
+	var showPlaybackInfo by remember { mutableStateOf(BuildConfig.DEBUG) }
 	var openLiveTvGuideOnStartConsumed by remember { mutableStateOf(false) }
 	var skipPromptTarget by remember { mutableStateOf<Duration?>(null) }
 	var endingSkipPromptVisible by remember { mutableStateOf(false) }
